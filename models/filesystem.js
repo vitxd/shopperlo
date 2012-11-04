5a5c2e666442dd9aec4a193d1107d7c447c0f196
vitxd <vitxd@yahoo.it>
2012-11-04 22:50:03 +0000

    Removed old files

--- models/filesystem.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-
-fs.exists       = fs.exists || require('path').exists;
-fs.existsSync   = fs.existsSync || require('path').existsSync;
-
-module.exports = fs;
--- models/login.js
+++
@@ -1,7 +0,0 @@
-exports.isLogged = function(req, res, next) {
-    if (!req.session.user_id) {
-        res.send('You are not authorized to view this page');
-    } else {
-        next();
-    }
-};
